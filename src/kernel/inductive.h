346480a27419fedebc2f05498a063fa3a0ff36bc
arthur-adjedj <arthur.adjedj@gmail.com>
2026-03-30 10:33:37 +0200

    chore: remove dead kernel code

--- src/kernel/inductive.h
+++ src/kernel/inductive.h
@@ -34,14 +34,6 @@ inline expr to_cnstr_when_K(environment const & env, recursor_val const & rval, 
     expr app_type    = whnf(infer_type(e));
     expr const & app_type_I = get_app_fn(app_type);
     if (!is_constant(app_type_I) || const_name(app_type_I) != rval.get_major_induct()) return e; // type incorrect
-    if (has_expr_mvar(app_type)) {
-        buffer<expr> app_type_args;
-        get_app_args(app_type, app_type_args);
-        for (unsigned i = rval.get_nparams(); i < app_type_args.size(); i++) {
-            if (has_expr_metavar(app_type_args[i]))
-                return e;
-        }
-    }
     optional<expr> new_cnstr_app = mk_nullary_cnstr(env, app_type, rval.get_nparams());
     if (!new_cnstr_app) return e;
     expr new_type    = infer_type(*new_cnstr_app);
